c3dee4a33f286bd34043053ff6ef04b67ce95d99
Rajdeep Sharma <rajdeep.sharma17@gmail.com>
2019-02-11 02:21:08 +0530

    Update application.py

--- server/application.py
+++ server/application.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # # -*- coding: utf-8 -*-
 from flask import Flask, render_template
-from flask import request, redirect, jsonify, url_for, flash
+from flask import request, redirect, jsonify
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
 from databaseSetup import Base, Members
@@ -9,11 +9,7 @@ from flask_cors import CORS
 from flask import session as login_session
 import random
 import string
-from oauth2client.client import flow_from_clientsecrets
-from oauth2client.client import FlowExchangeError
-import httplib2
 import json
-from flask import make_response
 import requests
 
 
